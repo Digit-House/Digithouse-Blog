ca8b72eb75cec349ca66dbf61e9610293296fe13
nyan lin tun <devkonyan@gmail.com>
2019-10-29 21:06:51 +0630

    Updates

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ self.__precacheManifest = [
     "url": "styles-3fc7741f5094743797e0.js"
   },
   {
-    "url": "commons-086cfc0a7209747cfe28.js"
+    "url": "commons-5fb8e36f8f0b19c78ed4.js"
   },
   {
     "url": "app-000f818894c25f4b5402.js"
@@ -45,31 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b8f5c5976ff3d2a89c8d2d8e68e08da1"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnLK3eRhf6.woff2",
-    "revision": "2ecb426f85ffc1c53b677556210e629f"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnPKreRhf6.woff2",
-    "revision": "b39aa336eef260975654fde0959de6fe"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnSKzeRhf6.woff2",
-    "revision": "5a2c789b59571ce97f51f9c75600f49b"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnZKveRhf6.woff2",
-    "revision": "d8bc03a60729f4b05b42e057e21eaed3"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9C4kDNxMZdWfMOD5Vn9LjJYTI.woff2",
-    "revision": "d67acf5dba25b4f92e3eadad20032199"
-  },
-  {
-    "url": "google-fonts/s/firasans/v10/va9E4kDNxMZdWfMOD5Vvl4jL.woff2",
-    "revision": "78773521b0ffe376bc7edd8ec2a591fb"
+    "revision": "757fbf3af354dafa0197a0ef30e1deb3"
   },
   {
     "url": "google-fonts/s/notosans/v9/o-0IIpQlx3QUlC5A4PNr5TRA.woff2",
@@ -86,26 +62,6 @@ self.__precacheManifest = [
   {
     "url": "google-fonts/s/padauk/v6/RrQSboJg-id7Onb512D005hF.woff2",
     "revision": "3a884322b11c29ccdfe72ad7358ce011"
-  },
-  {
-    "url": "google-fonts/s/poppins/v9/pxiByp8kv8JHgFVrLCz7Z1xlFQ.woff2",
-    "revision": "e535f7856b24153e0f3146e8f90a45c5"
-  },
-  {
-    "url": "google-fonts/s/poppins/v9/pxiByp8kv8JHgFVrLDz8Z1xlFQ.woff2",
-    "revision": "e8794816c5eaeaa9dd20a6d77ea3b272"
-  },
-  {
-    "url": "google-fonts/s/poppins/v9/pxiByp8kv8JHgFVrLEj6Z1xlFQ.woff2",
-    "revision": "aa4405ed937295296cf8510f437628e0"
-  },
-  {
-    "url": "google-fonts/s/poppins/v9/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2",
-    "revision": "61e2d96d01a7eba5ea3ec1bad7e736a8"
-  },
-  {
-    "url": "google-fonts/s/poppins/v9/pxiEyp8kv8JHgFVrJJfecg.woff2",
-    "revision": "1a280523d375e9358d5229df34fc8e94"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
